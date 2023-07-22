92c12362bdf8c76ef9ef3a747534df4f341dc453
lily4178993 <nellytelli@gmail.com>
2023-07-22 19:02:05 +0000

    Add Form to Books component

---
+++ src/components/Books.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from './form/Form';
+
+const Books = () => {
+    return (
+        <section>My Books
+            <Form />
+        </section>
+    );
+};
+
+export default Books;
