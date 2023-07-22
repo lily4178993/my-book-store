d69e5adf1957ee950059e5b0237b8725f27d1b73
lily4178993 <nellytelli@gmail.com>
2023-07-22 20:48:37 +0000

    Add CSS styles to books container

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Form from './form/Form';
 import BooksCard from './BooksCard';
 import booksData from './data/booksData';
+import BooksCSS from 'components/styles/Books.module.css';
 
 const Books = () => {
     return (
-        <section>
+        <section className={BooksCSS.BookstoreCMS}>
             
             <BooksCard booksData={booksData} />
+            <hr className={BooksCSS.Line} />
             <Form />
         </section>
     );
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -7,7 +7,7 @@ function Form() {
       <h2 className={FormCSS.Title}>Add New Book</h2>
       <form className={FormCSS.form}>
         <input type="text" placeholder="Book title"  className={FormCSS.LessonPanel} />
-        <select name="category" className={FormCSS.LessonPanel}>
+        <select name="category" className={FormCSS.LessonPanel2}>
             <option value="category1">category</option>
             <option value="category1">category1</option>
             <option value="category2">category2</option>
