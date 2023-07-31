2375f637ac221fe2ca9146ea9cbe9c93f412e123
lily4178993 <nellytelli@gmail.com>
2023-07-31 14:50:23 +0000

    Connect addBook reducer to its component

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,18 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from 'redux/books/booksSlice';
+import { v4 as uuid } from 'uuid';
+import Button from 'components/buttons/AddButton';
 import FormCSS from 'components/form/styles/Form.module.css';
 
 function Form() {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addBook({
+      item_id: uuid(), title, author, category,
+    }));
+
+    setTitle('');
+    setAuthor('');
+    setCategory('Business');
+  };
+
   return (
     <div>
       <h2 className={FormCSS.Title}>Add New Book</h2>
-      <form className={FormCSS.form}>
-        <input type="text" placeholder="Book title" className={FormCSS.LessonPanel} />
-        <select name="category" className={FormCSS.LessonPanel2}>
-          <option value="category1">category</option>
-          <option value="category1">category1</option>
-          <option value="category2">category2</option>
+      <form className={FormCSS.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Book title"
+          name="title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          className={FormCSS.LessonPanel}
+        />
+        <input
+          type="text"
+          placeholder="Book Author"
+          name="author"
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+          className={FormCSS.LessonPanel}
+        />
+        <select
+          name="category"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+          className={FormCSS.LessonPanel2}
+        >
+          <option value="business">Business</option>
+          <option value="computer-science">Computer Science</option>
+          <option value="education">Education</option>
+          <option value="engineering">Engineering</option>
+          <option value="fiction">Fiction</option>
+          <option value="humanities">Humanities</option>
+          <option value="JMSC">Journalism, Media Studies & Communication</option>
+          <option value="law">Law</option>
+          <option value="medecine">Medecine</option>
+          <option value="mathematics">Mathematics</option>
+          <option value="natural-sciences">Natural Sciences</option>
+          <option value="non-fiction">Non Fiction</option>
+          <option value="romancce">Romance</option>
+          <option value="social-sciences">Social Sciences</option>
+          <option value="student-success">Student Success</option>
         </select>
-        <button type="submit" title="Add Book" className={FormCSS.Rectangle2}>ADD BOOK</button>
+        <Button />
       </form>
     </div>
   );
